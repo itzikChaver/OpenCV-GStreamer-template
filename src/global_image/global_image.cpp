ffe842d3359bb590a45db4fdd6fc8184b828c67e
itzikChaver <ithikbar@gmail.com>
2024-08-01 13:24:30 +0300

    Added the ability to safely save the image in a global variable

---
+++ src/global_image/global_image.cpp
@@ -0,0 +1,31 @@
+#include "global_image.h"
+
+namespace GlobalImage 
+{
+    cv::Mat image;                          // Definition of the global image variable
+    std::mutex imageMutex;                  // Definition of the mutex for image access
+    std::condition_variable imageCondition; // Definition of the condition variable for image access
+
+    /**
+     * @brief Updates the global image with a new image.
+     * 
+     * @param cap The new image to be set.
+     */
+    void updateImage(const cv::Mat& cap) {
+        std::unique_lock<std::mutex> lock(imageMutex); // Lock the mutex before accessing the image
+        image = cap.clone(); // Update image from the video capture
+        imageCondition.notify_all(); // Notify all waiting threads that the image has been updated
+    }
+
+    /**
+     * @brief Safely retrieves the current global image.
+     * 
+     * This function ensures that the image is not being updated while it is being retrieved.
+     * 
+     * @return A clone of the current global image.
+     */
+    cv::Mat getImage() {
+        std::unique_lock<std::mutex> lock(imageMutex); // Lock the mutex before accessing the image
+        return image.clone(); // Return a clone of the current image
+    }
+}
---
+++ src/global_image/global_image.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <mutex>
+#include <condition_variable>
+
+namespace GlobalImage {
+    extern cv::Mat image; // Declare the global image variable
+    extern std::mutex imageMutex; // Declare the mutex for image access
+    extern std::condition_variable imageCondition; // Declare the condition variable for image access
+
+    /**
+     * @brief Updates the global image with a new image.
+     * 
+     * @param cap The new image to be set.
+     */
+    void updateImage(const cv::Mat& cap);
+
+    /**
+     * @brief Safely retrieves the current global image.
+     * 
+     * This function ensures that the image is not being updated while it is being retrieved.
+     * 
+     * @return A clone of the current global image.
+     */
+    cv::Mat getImage();
+}
